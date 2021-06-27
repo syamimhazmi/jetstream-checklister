df5197af8812dd63e22d0524ecb1b68ed91c2f71
syamimhazmi <syamimhazmi@gmail.com>
2021-06-27 20:51:56 +0800

    Change default of canLogin and canRegiter to dynamic value using Route facade

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,14 +6,15 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
         return Inertia::render('Welcome', [
-            'canLogin' => true,
-            'canRegister' => true,
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
             'laravelVersion' => Application::VERSION,
             'phpVersion' => PHP_VERSION,
         ]);
