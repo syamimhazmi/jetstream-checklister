13a022547e2d128762395465e9416b311ea2f118
syamimhazmi <syamimhazmi@gmail.com>
2021-10-03 00:35:42 +0800

    Update ChecklistGroupController.php
    Add model binding to the controller

--- app/Http/Controllers/Admin/ChecklistGroupController.php
+++ app/Http/Controllers/Admin/ChecklistGroupController.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ChecklistGroup;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ChecklistGroupController extends Controller
 {
@@ -14,7 +15,7 @@ class ChecklistGroupController extends Controller
      */
     public function index()
     {
-        //
+        Inertia::render('Admin/ChecklistGroup');
     }
 
     /**
@@ -41,10 +42,10 @@ class ChecklistGroupController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\ChecklistGroup  $checklistGroup
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(ChecklistGroup $checklistGroup)
     {
         //
     }
@@ -52,10 +53,10 @@ class ChecklistGroupController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\ChecklistGroup  $checklistGroup
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(ChecklistGroup $checklistGroup)
     {
         //
     }
@@ -64,10 +65,10 @@ class ChecklistGroupController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\ChecklistGroup  $checklistGroup
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, ChecklistGroup $checklistGroup)
     {
         //
     }
@@ -75,10 +76,10 @@ class ChecklistGroupController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\ChecklistGroup  $checklistGroup
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(ChecklistGroup $checklistGroup)
     {
         //
     }
